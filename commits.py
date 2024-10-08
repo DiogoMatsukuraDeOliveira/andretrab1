b7e82e661d1faf65bade959bf96a7122e7984435
Diogo Oliveira <diogoliveira2231@gmail.com>
2024-10-08 09:40:42 -0300

    7

--- commits.py
+++ commits.py
@@ -1,3 +1,11 @@
+import os
+
+def limpar_tela():
+    if os.name == 'nt':  
+        os.system('cls')
+    else:  
+        os.system('clear')
+#Peguei no chatgpt, não sabia como fazia isso#
 while True:
     print("Este é um software de calculos matemáticos, abaixo é o menu que exibe as opções para escolha de equação.")
     print("Selecione a operação:")
@@ -11,38 +19,38 @@ while True:
     menu = int(input("Escolha a opção que deseja: "))
 
     if menu == 1:
-       x = int(input("Qual o primeiro N?"))
-       y = int(input("Qual o segundo N?"))
+       x = int(input("Qual o primeiro N: "))
+       y = int(input("Qual o segundo N: "))
        result = x + y
        print("O resultado é: ",result)
 
     elif menu == 2:
-       x = int(input("Qual o primeiro N?"))
-       y = int(input("Qual o segundo N?"))
+       x = int(input("Qual o primeiro N: "))
+       y = int(input("Qual o segundo N: "))
        result = x - y
        print("O resultado é: ",result)
 
     elif menu == 3:
-       x = int(input("Qual o primeiro N?"))
-       y = int(input("Qual o segundo N?"))
+       x = int(input("Qual o primeiro N: "))
+       y = int(input("Qual o segundo N: "))
        result = x * y
        print("O resultado é: ",result)
 
     elif menu == 4:
-       x = int(input("Qual o primeiro N?"))
-       y = int(input("Qual o segundo N?"))
+       x = int(input("Qual o primeiro N: "))
+       y = int(input("Qual o segundo N: "))
        result = x / y
        print("O resultado é: ",result)
 
     elif menu == 5:
-       x = int(input("Qual o primeiro N?"))
-       y = int(input("Qual o segundo N?"))
+       x = int(input("Qual o primeiro N: "))
+       y = int(input("Qual o segundo N: "))
        result = x % y
        print("O resto da sua divisão é: ",result)   
 
     elif menu == 6:
-       x = int(input("Qual o primeiro N?"))
-       y = int(input("Qual o segundo N?"))
+       x = int(input("Qual o primeiro N: "))
+       y = int(input("Qual o segundo N: "))
        result1 = x * x
        result2 = y * y
        print("O resultado é: ",result1, "para n1 ao quadrado e ",result2, "para n2 ao quadrado.")
@@ -52,7 +60,7 @@ while True:
         break
 
     else:
-        print("Opção invalida, encerrando programa")
+        print("Opção invalida, encerrando programa...")
         break
 
     print("Deseja continuar utilizando o software?")
@@ -60,6 +68,8 @@ while True:
     print("2 - Não")
     continuar = int(input("Escolha a opção desejada: "))
     
-    if continuar != 1:
+    if continuar == 1:
+        limpar_tela()
+    else:
         print("Encerrando o programa.")
         break

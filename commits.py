4758c9e87e3851ad42edc8ea453aef8e6eb5db1e
Diogo Oliveira <diogoliveira2231@gmail.com>
2024-10-07 21:48:08 -0300

    2

--- commits.py
+++ commits.py
@@ -1,1 +1,11 @@
-print("teste")
+print("Este é um software de calculos matemáticos, abaixo é o menu que exibe as opções para escolha de equação.")
+print("Selecione a operação:")
+print("1. Soma")
+print("2. Subtração")
+print("3. Multiplicação")
+print("4. Divisão")
+print("5. Equação Quadrática")
+menu = int(input("Escolha a opção que deseja: "))
+
+if menu == 1:
+    

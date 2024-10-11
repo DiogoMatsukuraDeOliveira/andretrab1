05eaa66278f7ec4dd68811753f24988aafc3e015
Diogo Oliveira <diogoliveira2231@gmail.com>
2024-10-11 19:09:25 -0300

    11

--- commits.py
+++ commits.py
@@ -11,14 +11,7 @@ historico = []
 while True:
     print("Este é um software de calculos matemáticos, abaixo é o menu que exibe as opções para escolha de equação.")
     print("Selecione a operação:")
-    print("1 - Soma")
-    print("2 - Subtração")
-    print("3 - Multiplicação")
-    print("4 - Divisão interira")
-    print("5 - Divisão com resto")
-    print("6 - Equação Quadrática")
-    print("7 - Histórico")
-    print("0 - Sair do Programa")
+    pr
     print()
     menu = int(input("Escolha a opção que deseja: "))
 
